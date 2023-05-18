349220975d1f449c4ce24ad90151817de75f349a
ImDebuging <110248244+ImDebuging@users.noreply.github.com>
2023-05-18 10:21:48 +0800

    Create Floyd.cpp
    Floyd算法的C++描述

---
+++ Floyd.cpp
@@ -0,0 +1,25 @@
+const int MAXN = 1005; //最大点数
+int dist[MAXN][MAXN]; //dist[i][j]表示i到j的最短路
+
+void floyd(int n) {
+    //初始化
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j) {
+                dist[i][j] = 0;
+            } else {
+                dist[i][j] = INF;
+            }
+        }
+    }
+    //更新dist数组
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+}
